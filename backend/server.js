816a6cae3956e5cc9d7c004dab51767d6ede3383
Prashant Gangwar <105606164+Prashant9557@users.noreply.github.com>
2024-08-27 02:47:38 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,13 @@ const Message = require('./Models/Message');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
+
+app.use(cors({
+origin: ("https://deploy-chat-app.vercel.app"),
+  method:["POST", "GET"],
+  credentials: true
+}));
+
 app.use('/users', UserRoutes);
 
 require('./connection');
